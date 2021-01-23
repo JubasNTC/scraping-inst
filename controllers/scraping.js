1c97e0a162e73f3b67b61137dcd32fb44ff59b12
JubasNTC <jubas1337@mail.ru>
2021-01-23 15:26:42 +0300

    add support video stories scaping

--- controllers/scraping.js
+++ controllers/scraping.js
@@ -3,16 +3,35 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
+const ERROR_MESSAGE = {
+  error: 'Probably the link is not working or the wrong type is selected.',
+};
+
+const TYPES_LIST = new Set([
+  'photo',
+  'video',
+  'stories-photo',
+  'stories-video',
+]);
+
+const isValidParams = (url, type) =>
+  typeof url === 'string' && url.length > 0 && TYPES_LIST.has(type);
+
 const scrapeInstagram = async (req, res, next) => {
   const {
     body: { url, type },
   } = req;
 
+  if (!isValidParams(url, type)) {
+    return res.status(404).send(ERROR_MESSAGE);
+  }
+
+  const browser = await puppeteer.launch({
+    args: ['--incognito'],
+    headless: false,
+  });
+
   try {
-    const browser = await puppeteer.launch({
-      args: ['--incognito'],
-      headless: false,
-    });
     const page = await browser.newPage();
     await page.goto('https://www.instagram.com/accounts/login', {
       waitUntil: 'networkidle2',
@@ -24,11 +43,10 @@ const scrapeInstagram = async (req, res, next) => {
       delay: 20,
     });
     await page.click('button[type=submit]', { delay: 20 });
-
-    await page.waitFor(5000);
+    await page.waitForTimeout(5000);
 
     await page.goto(url, { waitUntil: 'networkidle2' });
-    await page.waitFor(3000);
+    await page.waitForTimeout(3000);
 
     let resultUrl = '';
 
@@ -45,29 +63,33 @@ const scrapeInstagram = async (req, res, next) => {
         resultUrl = $("meta[property='og:video']").attr('content');
         break;
 
-      case 'stories':
+      case 'stories-photo':
+        await page.click('button._42FBe', { delay: 20 });
+        await page.waitForTimeout(2000);
+        const photoStories = await page.evaluateHandle(() => {
+          return Array.from(document.getElementsByClassName('y-yJ5'))[0].src;
+        });
+        resultUrl = photoStories._remoteObject.value;
+        break;
+
+      case 'stories-video':
+        await page.click('button._42FBe', { delay: 20 });
+        await page.waitForTimeout(2000);
         const videoStories = await page.evaluateHandle(() => {
           return Array.from(document.getElementsByTagName('source'))[0].src;
         });
-        if (videoStories.length) {
-          resultUrl = videoStories;
-        } else {
-          const photoStories = await page.evaluateHandle(() => {
-            return Array.from(document.getElementsByTagName('img'))[0].src;
-          });
-          resultUrl = photoStories;
-        }
+        resultUrl = videoStories._remoteObject.value;
         break;
 
       default:
         break;
     }
 
-    await browser.close();
-
-    return res.status(200).send({ url: resultUrl });
+    res.status(200).send({ url: resultUrl });
   } catch (e) {
-    return res.status(400).send(e);
+    res.status(400).send(ERROR_MESSAGE);
+  } finally {
+    await browser.close();
   }
 };
 
